a26ce5294d4a121fcb28bd4c660e846d3fe2e922
Tung Dao <daotung492@gmail.com>
2023-10-29 02:32:17 +0700

    add admin template

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,6 @@ Route::get('/', function () {
 Route::get('test', function () {
     return view('test');
 });
-Route::post("PaymentService.php", function () {
-    return view('PaymentService');
-});
 Route::get('index.html', function () {
     return view('index');
 });
@@ -35,3 +32,20 @@ Route::view('/about.html', 'about');
 Route::view('/blog.html', 'blog');
 Route::view('/blog-single.html', 'blog-single');
 Route::view('/contact.html', 'contact');
+
+
+// admin
+Route::group(['prefix' => '/admin'], function () {
+    // Các route trong thư mục admin
+    Route::get('/admin', function () {
+        return view('admin/dashboard');
+    });
+    Route::view('/admin/billing.html', 'billing');
+    Route::view('/admin/dashboard.html', 'dashboard');
+    Route::view('/admin/profile.html', 'profile');
+    Route::view('/admin/rtf.html', 'rtf');
+    Route::view('/admin/sign-in.html', 'sign-in');
+    Route::view('/admin/sign-up.html', 'sign-up');
+    Route::view('/admin/tables.html', 'tables');
+    Route::view('/admin/virtual-reality.html', 'virtual-reality');
+});
